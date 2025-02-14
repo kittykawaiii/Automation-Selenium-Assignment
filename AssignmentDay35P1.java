d0cf47e2bcf35a6eea408cc3c763d4e0300a1e67
Kittykawaii <phamdieulyy2309@gmail.com>
2025-02-14 14:04:17 +0700

    Update and rename Assignment_day35_1.java to AssignmentDay35P1.java

--- AssignmentDay35P1.java
+++ AssignmentDay35P1.java
@@ -18,22 +18,22 @@ public class Assignment_day35_1 {
 
         // Double click
 
-        String input_text ="kawaii";
+        String inputText ="kawaii";
 
-        WebElement input_box1 = driver.findElement(By.xpath("//input[@id='field1']"));
+        WebElement inputBox1 = driver.findElement(By.xpath("//input[@id='field1']"));
         String currentValue = input_box1.getAttribute("value");
         if (currentValue != null && !currentValue.isEmpty()) {
-            input_box1.clear();
+            inputBox1.clear();
         }
-        input_box1.sendKeys(input_text);
+        inputBox1.sendKeys(inputText);
 
         WebElement button = driver.findElement(By.xpath("//button[normalize-space()='Copy Text']"));
 
         Actions act = new Actions(driver);
         act.doubleClick(button).perform();
 
-        WebElement input_box2 = driver.findElement(By.xpath("//input[@id='field2']"));
-        String copiedValue = input_box2.getAttribute("value");
+        WebElement inputBox2 = driver.findElement(By.xpath("//input[@id='field2']"));
+        String copiedValue = inputBox2.getAttribute("value");
 
         if (copiedValue.equals(input_text)){
             System.out.println("Button worked!");
@@ -44,16 +44,16 @@ public class Assignment_day35_1 {
 
         // Drag & Drop
 
-        Actions act_drag=new Actions(driver);
+        Actions actDrag=new Actions(driver);
 
         WebElement drag = driver.findElement(By.xpath("//div[@id='draggable']"));
         WebElement drop = driver.findElement(By.xpath("//div[@id='droppable']"));
 
-        act_drag.dragAndDrop(drag,drop).perform();
+        actDrag.dragAndDrop(drag,drop).perform();
 
-        WebElement drop_text = driver.findElement(By.xpath("//p[normalize-space()='Dropped!']"));
+        WebElement dropText = driver.findElement(By.xpath("//p[normalize-space()='Dropped!']"));
 
-        if (drop_text.getText().equals("Dropped!")){
+        if (dropText.getText().equals("Dropped!")){
             System.out.println("Drop successful!");
         }
         else {
